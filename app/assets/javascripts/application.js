ed5dea4aa6cbd3f4c2a01fe9dad2c8a8ed40259f
linh0110 <lvlthpttt98@gmail.com>
2020-07-08 19:13:57 +0700

    fix view details and quantity

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,19 +17,37 @@
 //= require turbolinks
 //= require_tree .
 
-function incrementValue() {
-    var value = parseInt(document.getElementById('number').value);
-    value = isNaN(value) ? 0 : value;
-    value++;
-    document.getElementById('number').value = value;
-}
+// function incrementValue() {
+//     var value = parseInt(document.getElementById('number').value);
+//     var name = $('.input-number').attr('name');
+//     value = isNaN(value) ? 0 : value;
+//     var maxValue = parseInt($('.input-number').attr('max'));                
+//     if(!maxValue) maxValue = 200; 
+//     if(value < maxValue) {                      
+//         value++;
+//         document.getElementById('number').value = value;       
+//         $(".btn-number[data-type='plus'][data-field='"+name+"']").removeAttr('disabled')       
+//     }                
+//     else if (value == maxValue) {                    
+//         $(".btn-number[data-type='plus'][data-field='"+name+"']").attr('disabled', true);          
+//     }        
+// }
 
-function decrementValue() {
-    var value = parseInt(document.getElementById('number').value);
-    value = isNaN(value) ? 0 : value;
-    value--;
-    document.getElementById('number').value = value;
-}
+// function decrementValue() {
+//     var value = parseInt(document.getElementById('number').value);
+//     var name = $('.input-number').attr('name');
+//     value = isNaN(value) ? 0 : value;
+//     var minValue = parseInt($('.input-number').attr('min'));                 
+//     if(!minValue) minValue = 1;                
+//     if(value > minValue) {                    
+//         value--;
+//         document.getElementById('number').value = value;   
+//         $(".btn-number[data-type='minus'][data-field='"+name+"']").removeAttr('disabled')          
+//     }                 
+//     else if (value == minValue) {                    
+//         $(".btn-number[data-type='minus'][data-field='"+name+"']").attr('disabled', true);     
+//     }       
+// }
 
 $(function(){
     $('#formSize input').on('change', function() {
