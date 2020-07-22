e2a99edad1c23d3d7c1835d5d22d7f526507d919
linh0110 <lvlthpttt98@gmail.com>
2020-07-22 16:53:36 +0700

    view admin

---
+++ app/admin/chitietdathangs.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Chitietdathang do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :chitietsp_id, :dondathang_id, :soluong, :thanhtien
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:chitietsp_id, :dondathang_id, :soluong, :thanhtien]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
